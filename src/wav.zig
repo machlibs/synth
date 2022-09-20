061aa8d7dcf8ca76061d6364d3d70bc871acc4c7
Louis Pearson <opensource@louispearson.work>
2022-09-19 23:32:16 -0600

    Improve WAV file decoder

--- src/wav.zig
+++ src/wav.zig
@@ -1,15 +1,84 @@
 const std = @import("std");
 const testing = std.testing;
 
-const Riff = struct {
+const WAV = @This();
+
+format: Format,
+samples: WavData,
+
+/// Read wav file from given reader
+pub fn read(alloc: std.mem.Allocator, reader: anytype) !WAV {
+    // Read RIFF header to verify it is a WAV file
+    const riff = try readHeader(reader);
+    _ = riff;
+
+    // Read format data
+    const format = try readFormat(reader);
+
+    // Read data header and allocate
+    const data_header = try readDataHeader(reader);
+    const data_format = try format.getDataFormat();
+    const sample_num = getSampleNum(format, data_header);
+    const data_buffer: WavData = switch (data_format) {
+        .UInt8 => .{ .UInt8 = try alloc.alloc(u8, sample_num) },
+        .SInt16 => .{ .SInt16 = try alloc.alloc(i16, sample_num) },
+    };
+
+    try readAllData(reader, format, data_header, data_buffer);
+
+    return WAV{
+        .format = format,
+        .samples = data_buffer,
+    };
+}
+
+/// Read a WAV file from memory
+pub fn readFromBuffer(alloc: std.mem.Allocator, data: []const u8) !WAV {
+    var fixed_buffer_stream = std.io.FixedBufferStream([]const u8){
+        .buffer = data,
+        .pos = 0,
+    };
+
+    const reader = fixed_buffer_stream.reader();
+
+    return WAV.read(alloc, reader);
+}
+
+/// Free the wav samples
+pub fn free(wav: *WAV, alloc: std.mem.Allocator) void {
+    wav.samples.free(alloc);
+}
+
+/// Convert the u8 or i16 data into floating point samples
+pub fn toF32(wav: *WAV, out: []f32) usize {
+    switch (wav.samples) {
+        .UInt8 => |slice| {
+            if (out.len < slice.len) return error.OutOfMemory;
+            for (slice) |sample, i| {
+                out[i] = (@intToFloat(f32, sample) / @as(f32, std.math.maxInt(u8))) * 2 - 1;
+            }
+        },
+        .SInt16 => |slice| {
+            if (out.len < slice.len) return error.OutOfMemory;
+            for (slice) |sample, i| {
+                out[i] = @intToFloat(f32, sample) / @as(f32, std.math.maxInt(i16));
+            }
+        },
+    }
+}
+
+/// Stores the RIFF header information for a WAV file
+pub const Riff = struct {
     chunk_id: [4]u8,
     chunk_size: u32,
     format: [4]u8,
 };
 
+/// WAV file format values. Only PCM is supported at the moment.
 const AudioFormat = enum(u16) { PCM = 1 };
 
-const Format = struct {
+/// WAV Foramt chunk, describes the format of the audio data
+pub const Format = struct {
     subchunk_id: [4]u8,
     subchunk_size: u32,
     audio_format: AudioFormat,
@@ -21,46 +90,39 @@ const Format = struct {
 
     pub fn getDataFormat(fmt: Format) !WavDataEnum {
         if (fmt.bits_per_sample == 8) {
-            if (fmt.num_channels == 1) return .Mono8Bit;
-            if (fmt.num_channels == 2) return .Stereo8Bit;
-            return error.Unsupported;
+            return .UInt8;
         } else if (fmt.bits_per_sample == 16) {
-            if (fmt.num_channels == 1) return .Mono16Bit;
-            if (fmt.num_channels == 2) return .Stereo16Bit;
-            return error.Unsupported;
+            return .SInt16;
         }
         return error.Unsupported;
     }
 };
 
-const DataHeader = struct {
+/// Header for the audio data chunk
+pub const DataHeader = struct {
     subchunk_id: [4]u8,
     subchunk_size: u32,
 };
 
-const WavDataEnum = enum {
-    Mono8Bit,
-    Stereo8Bit,
-    Mono16Bit,
-    Stereo16Bit,
+/// Possible formats for the audio stream
+pub const WavDataEnum = enum {
+    UInt8,
+    SInt16,
 };
 
-const WavData = union(WavDataEnum) {
-    Mono8Bit: []u8,
-    Stereo8Bit: []u8,
-    Mono16Bit: []u16,
-    Stereo16Bit: []u16,
+pub const WavData = union(WavDataEnum) {
+    UInt8: []u8,
+    SInt16: []i16,
 
     pub fn free(data: WavData, alloc: std.mem.Allocator) void {
         switch (data) {
-            .Mono8Bit => |slice| alloc.free(slice),
-            .Stereo8Bit => |slice| alloc.free(slice),
-            .Mono16Bit => |slice| alloc.free(slice),
-            .Stereo16Bit => |slice| alloc.free(slice),
+            .UInt8 => |slice| alloc.free(slice),
+            .SInt16 => |slice| alloc.free(slice),
         }
     }
 };
 
+/// Read the RIFF header chunk of a WAV file.
 fn readHeader(reader: anytype) !Riff {
     var riff: Riff = undefined;
     // -- Header -- //
@@ -78,6 +140,7 @@ fn readHeader(reader: anytype) !Riff {
     return riff;
 }
 
+/// Read the format sub-chunk of a WAV file.
 fn readFormat(reader: anytype) !Format {
     var format: Format = undefined;
 
@@ -107,6 +170,7 @@ fn readFormat(reader: anytype) !Format {
     return format;
 }
 
+/// Read the data header to check validity and find the data length
 fn readDataHeader(reader: anytype) !DataHeader {
     var data_header: DataHeader = undefined;
     // -- Sub-chunk 2 -- //
@@ -118,45 +182,22 @@ fn readDataHeader(reader: anytype) !DataHeader {
     return data_header;
 }
 
+/// Read all the data from the WAV data section
 fn readAllData(reader: anytype, format: Format, data_header: DataHeader, data: WavData) !void {
-    const sample_num = getSampleNum(format, data_header);
+    const num_samples = getSampleNum(format, data_header);
     switch (data) {
-        .Mono8Bit => |slice| {
-            const num_samples = data_header.subchunk_size / (format.num_channels * format.bits_per_sample / 8);
+        .UInt8 => |slice| {
             if (slice.len < num_samples) return error.InsufficientBuffer;
             var i: usize = 0;
-            while (i < sample_num) : (i += 1) {
+            while (i < num_samples) : (i += 1) {
                 slice[i] = try reader.readInt(u8, .Little);
             }
         },
-        .Stereo8Bit => |slice| {
-            const num_samples = data_header.subchunk_size / (format.num_channels * format.bits_per_sample / 8);
+        .SInt16 => |slice| {
             if (slice.len < num_samples) return error.InsufficientBuffer;
-            // return try reader.readAll(@ptrCast([]u8, slice));
-            // try reader.readAll(slice);
             var i: usize = 0;
-            while (i < sample_num) : (i += 1) {
-                slice[i] = try reader.readInt(u8, .Little);
-            }
-        },
-        .Mono16Bit => |slice| {
-            const num_samples = data_header.subchunk_size / (format.num_channels * format.bits_per_sample / 8);
-            if (slice.len < num_samples) return error.InsufficientBuffer;
-            // return try reader.readAll(@ptrCast([]u8, slice));
-            // try reader.readAll(slice);
-            var i: usize = 0;
-            while (i < sample_num) : (i += 1) {
-                slice[i] = try reader.readInt(u16, .Little);
-            }
-        },
-        .Stereo16Bit => |slice| {
-            const num_samples = data_header.subchunk_size / (format.num_channels * format.bits_per_sample / 8);
-            if (slice.len < num_samples) return error.InsufficientBuffer;
-            // return try reader.readAll(@ptrCast([]u8, slice));
-            // try reader.readAll(slice);
-            var i: usize = 0;
-            while (i < sample_num) : (i += 1) {
-                slice[i] = try reader.readInt(u16, .Little);
+            while (i < num_samples) : (i += 1) {
+                slice[i] = try reader.readInt(i16, .Little);
             }
         },
     }
@@ -166,7 +207,7 @@ fn getSampleNum(format: Format, data_header: DataHeader) usize {
     return data_header.subchunk_size / (format.num_channels * format.bits_per_sample / 8);
 }
 
-test "ambience.wav" {
+test "manual read ambience.wav" {
     const file = @embedFile("ambience.wav");
     var fixed_buffer_stream = std.io.FixedBufferStream([]const u8){
         .buffer = file,
@@ -195,12 +236,26 @@ test "ambience.wav" {
     const sample_num = getSampleNum(format, data_header);
     const alloc = testing.allocator;
     const data_buffer: WavData = switch (data_format) {
-        .Mono8Bit => .{ .Mono8Bit = try alloc.alloc(u8, sample_num) },
-        .Stereo8Bit => .{ .Stereo8Bit = try alloc.alloc(u8, sample_num) },
-        .Mono16Bit => .{ .Mono16Bit = try alloc.alloc(u16, sample_num) },
-        .Stereo16Bit => .{ .Stereo16Bit = try alloc.alloc(u16, sample_num) },
+        .UInt8 => .{ .UInt8 = try alloc.alloc(u8, sample_num) },
+        .SInt16 => .{ .SInt16 = try alloc.alloc(i16, sample_num) },
     };
     defer data_buffer.free(alloc);
 
     try readAllData(reader, format, data_header, data_buffer);
+}
+
+test "read ambience.wav" {
+    const file = @embedFile("ambience.wav");
+    var wav = try WAV.readFromBuffer(testing.allocator, file);
+    defer wav.free(testing.allocator);
+
+    const format = wav.format;
+    try testing.expectEqualSlices(u8, "fmt ", &format.subchunk_id);
+    try testing.expectEqual(@as(u32, 16), format.subchunk_size);
+    try testing.expectEqual(AudioFormat.PCM, format.audio_format);
+    try testing.expectEqual(@as(u16, 2), format.num_channels);
+    try testing.expectEqual(@as(u32, 44100), format.sample_rate);
+    try testing.expectEqual(@as(u32, 176400), format.byte_rate);
+    try testing.expectEqual(@as(u32, 4), format.block_align);
+    try testing.expectEqual(@as(u16, 16), format.bits_per_sample);
 }
